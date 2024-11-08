69c3f5dda61b6f2b97d9b77065b8792bdc93188b
Diego Santos <diegoleonardo@outlook.com>
2024-11-08 16:13:47 -0300

    project start

---
+++ src/transaction_authorizer/core.clj
@@ -0,0 +1,12 @@
+(ns transaction-authorizer.core
+  "FIXME: my new org.corfield.new/scratch project.")
+
+(defn exec
+  "Invoke me with clojure -X transaction-authorizer.core/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m transaction-authorizer.core"
+  [& args]
+  (println "-main with" args))
